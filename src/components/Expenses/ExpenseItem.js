1ede13b18628b015881d3dad892092f7fa2a26e4
Jameson <90803725+jamesonarnett@users.noreply.github.com>
2021-12-11 19:27:11 -0600

    Add form item to list. Passing state practice!

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  let [title, titleHandler] = useState(props.title);
+
+  const clickHandler = () => {
+    titleHandler("updated");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change title</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
